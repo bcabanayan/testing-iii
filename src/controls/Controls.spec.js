d2bd9d6cbe739c9371c0457322e06c08920e5665
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-04 21:18:05 -0700

    Added tests to control component to ensure lock button toggles successfully upon initial closure

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -10,24 +10,69 @@ import Controls from './Controls.js';
 afterEach(cleanup);
 
 describe('The Controls component', () => {
-    // tests for close button
     it('provides a button to close the gate', () => {
         const { getByText } = render(<Controls/>);
         getByText(/close gate/i);
+    });
+
+    it('provides a button to lock the gate', () => {
+        const { getByText } = render(<Controls/>);
+        getByText(/lock gate/i);
     });
 
     it('button to close gate switches to open gate after closing', () => {
         render(<Dashboard/>);
         const component = render(<Controls/>);
         const openButton = component.getByText(/close gate/i);
-        
+
         fireEvent.click(openButton);
 
         expect(openButton).toHaveTextContent(/open gate/i);
     });
 
-    it('provides a button to lock the gate', () => {
-        const { getByText } = render(<Controls/>);
-        getByText(/lock gate/i);
+    it('clicking open button after closing toggles button back to close option', () => {
+        render(<Dashboard/>);
+        const component = render(<Controls/>);
+        const openButton = component.getByText(/close gate/i);
+
+        fireEvent.click(openButton);
+        fireEvent.click(openButton);
+
+        expect(openButton).toHaveTextContent(/close gate/i);
+    });
+
+    it('cannot lock gate without closing gate first', () => {
+        render(<Dashboard/>);
+        const component = render(<Controls/>);
+        const lockButton = component.getByText(/lock gate/i);
+
+        fireEvent.click(lockButton);
+
+        expect(lockButton).toHaveTextContent(/lock gate/i);
+    });
+
+    it('can lock gate after closing', () => {
+        render(<Dashboard/>);
+        const component = render(<Controls/>);
+        const openButton = component.getByText(/close gate/i);
+        const lockButton = component.getByText(/lock gate/i);
+
+        fireEvent.click(openButton);
+        fireEvent.click(lockButton);
+
+        expect(lockButton).toHaveTextContent(/unlock gate/i);
+    });
+
+    it('can unlock gate after locking', () => {
+        render(<Dashboard/>);
+        const component = render(<Controls/>);
+        const openButton = component.getByText(/close gate/i);
+        const lockButton = component.getByText(/lock gate/i);
+
+        fireEvent.click(openButton);
+        fireEvent.click(lockButton);
+        fireEvent.click(lockButton);
+
+        expect(lockButton).toHaveTextContent(/lock gate/i);
     });
 });
