1bf188fb373cc0c957e0b4501517711b3a457a0f
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-05 19:42:10 -0700

    Tested open/close indicator based on button clicking

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,6 +6,8 @@ import Display from './Display.js';
 import Dashboard from '../dashboard/Dashboard.js'
 import Controls from '../controls/Controls.js';
 
+afterEach(cleanup);
+
 describe('The Display component', () => {
     it('provides an indicator that gate is open by default', () => {
         const { getByText } = render(<Display/>);
@@ -15,5 +17,30 @@ describe('The Display component', () => {
     it('provides an indicator that gate is unlocked by default', () => {
         const { getByText } = render(<Display/>);
         getByText(/unlocked/i);
+    });
+
+    it('displays closed when the closed button is clicked', () => {
+        render(<Dashboard/>);
+        const controls = render(<Controls/>);
+        const closeButton = controls.getByText(/close gate/i);
+        const display = render(<Display/>);
+        const closedIndicator = display.getByText(/open/i);
+
+        fireEvent.click(closeButton);
+
+        expect(closedIndicator).toHaveTextContent(/closed/i);
+    });
+
+    it('displays open when the open button is clicked', () => {
+        render(<Dashboard/>);
+        const controls = render(<Controls/>);
+        const closeButton = controls.getByText(/close gate/i);
+        const display = render(<Display/>);
+        const openIndicator = display.getByText(/open/i);
+
+        fireEvent.click(closeButton);
+        fireEvent.click(closeButton);
+
+        expect(openIndicator).toHaveTextContent(/open/i);
     });
 });
